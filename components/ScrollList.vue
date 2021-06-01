6d1e626f496f728c309497b5e01c6e5389fb19f0
Gero <melirandeu@gmail.com>
2021-06-01 14:46:42 +0900

    [add]見た目を形成

--- components/ScrollList.vue
+++ components/ScrollList.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <div v-for="(name, index) in lines" :key="index">
-      <h2 class="name">{{ name }}</h2>
-    </div>
+    <ul class="linelist" ref="linelist">
+      <li class="line" v-for="(name, index) in lines" :key="index" name='line'>
+        <h2 class="name">{{ name }}</h2>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+
 import {mapState} from 'vuex';
 export default {
   data: function() {
@@ -14,14 +17,30 @@ export default {
       content:""
     }
   },
+  mounted(){
+    this.Tilt()
+  },
   computed: {
     ...mapState(['lines'])
-  }
+  },
+  methods:{
+    Tilt(){
+      var forEach = Array.prototype.forEach;
+      var  value= 0
+      let linelist=document.getElementsByName('line')
+      forEach.call(linelist,function (elem) {
+        elem.style.marginLeft = value+"px"
+        value=value+20
+        console.log(value+"px")
+      })
+    }
+  },
 }
 </script>
 
 <style>
 .name{
-  font-size:50px;
+  font-size:45px;
 }
+
 </style>
